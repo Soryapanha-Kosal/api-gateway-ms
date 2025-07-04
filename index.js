fda6f1e1e8eaba56bfabee05d3a74dedbfbbbd50
Soryapanha <mikekosal12@gmail.com>
2025-07-04 22:26:29 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+
+app.use('/student', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
+app.use('/teacher', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+
+app.listen(3000, () => console.log("API Gateway running on port 3000"));
