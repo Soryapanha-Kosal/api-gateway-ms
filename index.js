51e20298a305585232eaa54b181a2b0ceae6746c
Soryapanha <mikekosal12@gmail.com>
2025-07-04 23:19:15 +0700

    replace localhost with example EC2 IP

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
 
-app.use('/student', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
-app.use('/teacher', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+// Update with actual IPs of your EC2 instances running Student and Teacher
+app.use('/student', createProxyMiddleware({
+  target: 'http://54.91.176.127:3001',
+  changeOrigin: true
+}));
 
-app.listen(3000, () => console.log("API Gateway running on port 3000"));
+app.use('/teacher', createProxyMiddleware({
+  target: 'http://98.80.15.4:3002',
+  changeOrigin: true
+}));
+
+app.listen(3000, () => console.log('API Gateway running on port 3000'));
